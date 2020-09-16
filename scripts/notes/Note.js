9a297862a339ad3900273e5aaa8556d52ac541c0
Aaron Gertler <aaron.gertler@gmail.com>
2020-09-16 13:29:18 -0500

    fixed note module posting, repointed eventHub

--- scripts/notes/Note.js
+++ scripts/notes/Note.js
@@ -1,13 +1,11 @@
 // take notes and display on DOM
 
-const eventHub = document.querySelector(".container")
-
 export const NoteHTMLConverter = (noteObj) => {
     return `
         <section class="note">
-            <div class="note--title">Criminal: ${noteObj.suspect}</div>
+            <div class="note--title"><strong>Criminal:</strong> ${noteObj.suspect}</div>
             <div class="note--content">${noteObj.noteText}</div>
-            <div class="note--timestamp">Timestamp: ${new Date(noteObj.date).toLocaleDateString('en-US')}</div>
+            <div class="note--timestamp"><strong>Timestamp:</strong> ${new Date(noteObj.date).toLocaleDateString('en-US')}</div>
         </section>
     `
 }
