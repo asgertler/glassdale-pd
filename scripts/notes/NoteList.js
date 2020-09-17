8c61b526c4307cbf40277dd1b9b060f59090880e
Aaron Gertler <aaron.gertler@gmail.com>
2020-09-17 16:23:30 -0500

    updated NoteForm and NoteList code

--- scripts/notes/NoteList.js
+++ scripts/notes/NoteList.js
@@ -1,23 +1,38 @@
 /*
-    map over an array and siplay all notes from Note.js
+    map over an array and display all notes from Note.js
 */
 
+import { getCriminals, useCriminals } from '../criminals/CriminalProvider.js'
 import { getNotes, useNotes } from './NoteProvider.js'
-import { NoteHTMLConverter } from './Note.js'
+// import { NoteHTMLConverter } from './Note.js'
 
 const eventHub = document.querySelector(".container")
 const contentTarget = document.querySelector("#noteListContainer")
 
-const render = (notes) => {
-    contentTarget.innerHTML = notes.map((noteObj) => {
-        return NoteHTMLConverter(noteObj)
-    }).join("")
+const render = (noteCollection, criminalCollection) => {
+    contentTarget.innerHTML = noteCollection.map((note) => {
+
+        // Find the related criminal
+        const relatedCriminal = criminalCollection.find(criminal => criminal.id === note.criminalId)
+
+        return `
+            <section class="note">
+                <h2>Note about ${relatedCriminal.name}</h2>
+                ${note.noteText}
+            </section>
+        `
+    })
 }
 
 export const NoteList = () => {
     getNotes()
-        .then(useNotes)
-        .then(render)
+        .then(getCriminals)
+        .then(() => {
+            const notes = useNotes()
+            const criminals = useCriminals()
+
+            render(notes, criminals)
+        })
 }
 
 eventHub.addEventListener("noteStateChanged", () => {
